4d4d1e6cf16dc267cb08f694c4dd4920a741409b
itesic <ilija.tesic@gmail.com>
2016-08-20 23:22:18 +0200

    Tape equilibrium

---
+++ 3-TimeComplexity/tests/TapeEquilibrium.spec.js
@@ -0,0 +1,14 @@
+var chai = require('chai');
+var expect = chai.expect;
+var TapeEquilibrium = require('../TapeEquilibrium');
+
+describe('TapeEquilibrium', function () {
+
+    it('TapeEquilibrium(A) should be 1 for array [3,1,2,4,3]', function (){
+        expect(TapeEquilibrium([3,1,2,4,3])).to.equal(1);
+    });
+
+    it('TapeEquilibrium(A) should be 2000 for array [1000, -1000]', function (){
+        expect(TapeEquilibrium([1000, -1000])).to.equal(2000);
+    });
+});
