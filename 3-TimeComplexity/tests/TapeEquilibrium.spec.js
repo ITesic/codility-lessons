24ccefcdb87a33036c3e2da7e0bfa302b5b87fda
itesic <ilija.tesic@gmail.com>
2016-08-21 00:40:45 +0200

    FrogJmp

--- 3-TimeComplexity/tests/TapeEquilibrium.spec.js
+++ 3-TimeComplexity/tests/TapeEquilibrium.spec.js
@@ -4,11 +4,11 @@ var TapeEquilibrium = require('../TapeEquilibrium');
 
 describe('TapeEquilibrium', function () {
 
-    it('TapeEquilibrium(A) should be 1 for array [3,1,2,4,3]', function (){
+    it('should be 1 for array [3,1,2,4,3]', function (){
         expect(TapeEquilibrium([3,1,2,4,3])).to.equal(1);
     });
 
-    it('TapeEquilibrium(A) should be 2000 for array [1000, -1000]', function (){
+    it('should be 2000 for array [1000, -1000]', function (){
         expect(TapeEquilibrium([1000, -1000])).to.equal(2000);
     });
 });
